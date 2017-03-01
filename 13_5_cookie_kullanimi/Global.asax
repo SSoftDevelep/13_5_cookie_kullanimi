7195b3f2c04cb58420dccc072588cfb1fd900033
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-01 16:24:48 +0300

    Add project files.

---
+++ 13_5_cookie_kullanimi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_13_5_cookie_kullanimi.MvcApplication" Language="C#" %>
---
+++ 13_5_cookie_kullanimi/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace _13_5_cookie_kullanimi
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
